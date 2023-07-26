9ddb22c4997bf5b8980ccce109cc8eff36ef658f
Emiliano Cararo <emicararo92@gmail.com>
2023-07-26 15:06:29 -0300

    final

--- JS/tienda.js
+++ JS/tienda.js
@@ -1,31 +1,28 @@
-
-// pre - tienda e-commerce
-
 class Producto {
     constructor(modelo, talle, precio) {
-        this.modelo = modelo
-        this.talle = talle
-        this.precio = precio
-        this.info = `${this.modelo}, talle ${this.talle}, precio : ${this.precio}`
+      this.modelo = modelo;
+      this.talle = talle;
+      this.precio = precio;
     }
+  
     verInfo() {
-        console.log(this.info);
+      return `Modelo: ${this.modelo}, Talle: ${this.talle}, Precio: $${this.precio}`;
     }
-}
-
-// productos disponibles para compras
-
-let joggingBelgrano = new Producto("Joggin Belgrano", "S", 5000);
-
-let remeraBelgrano = new Producto("Remera Belgrano 2023", "L", 4600)
-
-let remeraNiñoBelgrano = new Producto("Remera niño Belgrano", "16", 3500)
-
-let conjuntoNiñoBelgrano = new Producto("Conjunto niño Belgrano", "12", 8500)
-
-
-
-joggingBelgrano.verInfo()
-remeraBelgrano.verInfo()
-remeraNiñoBelgrano.verInfo()
-conjuntoNiñoBelgrano.verInfo()
+  }
+  
+ 
+  let joggingBelgrano = new Producto("Joggin Belgrano", "S", 5000);
+  let remeraBelgrano = new Producto("Remera Belgrano 2023", "L", 4600);
+  let remeraNiñoBelgrano = new Producto("Remera niño Belgrano", "16", 3500);
+  let conjuntoNiñoBelgrano = new Producto ("Conjunto niño Belgrano", "14", 8000)
+  
+  
+  let carrito = [joggingBelgrano, remeraBelgrano, remeraNiñoBelgrano, conjuntoNiñoBelgrano];
+  
+  
+  console.log("Carrito de compras:");
+  carrito.forEach((producto) => console.log(producto.verInfo()));
+  
+  
+  let total = carrito.reduce((totalAcumulado, producto) => totalAcumulado + producto.precio, 0);
+  console.log("Total: $" + total);
